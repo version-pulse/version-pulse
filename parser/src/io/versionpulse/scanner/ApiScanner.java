78537d7795486d6d057d0983f5c4e91da5e9a660
kwonssshyeon <kwonsh4396@gmail.com>
2025-03-22 20:48:11 +0900

    [Parser] Feat: API 단위 스캔 구현

--- parser/src/io/versionpulse/scanner/ApiScanner.java
+++ parser/src/io/versionpulse/scanner/ApiScanner.java
@@ -1,5 +1,35 @@
 package io.versionpulse.scanner;
 
-public class ApiScanner {
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
+import io.versionpulse.annotation.Api;
+
+
+public class ApiScanner {
+	
+	public List<Method> execute(Class<?> clazz) {
+		List<Method> result = new ArrayList<>();
+		
+		Method[] methods = clazz.getDeclaredMethods();
+		for (Method method : methods) {
+			if (isTargetMethod(method)) {
+				result.add(method);
+			}
+		}
+		return result;
+	}
+	
+	// Api 어노테이션이 있는 메소드만 포함
+	private boolean isTargetMethod(Method method) {
+		Annotation[] annotations = method.getDeclaredAnnotations();
+		for (Annotation annotation : annotations) {
+			if (annotation instanceof Api) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- parser/src/test/java/ScannerTest.java
+++ parser/src/test/java/ScannerTest.java
@@ -1,19 +1,25 @@
 package test.java;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 import org.junit.Test;
 
 import io.versionpulse.scanner.ApiGroupScanner;
+import io.versionpulse.scanner.ApiScanner;
 
 public class ScannerTest {
 	@Test
 	public void test() {
-		System.out.println("테스트");
 		ApiGroupScanner groupScanner = new ApiGroupScanner("io.versionpulse");
-		List<Class<?>> result = groupScanner.execute();
-		for (Class<?> clazz : result) {
+		List<Class<?>> classes = groupScanner.execute();
+		for (Class<?> clazz : classes) {
 			System.out.println(clazz);
+			ApiScanner apiScanner = new ApiScanner();
+			List<Method> methods = apiScanner.execute(clazz);
+			for (Method method : methods) {
+				System.out.println(method);
+			}
 		}
 	}
 }
