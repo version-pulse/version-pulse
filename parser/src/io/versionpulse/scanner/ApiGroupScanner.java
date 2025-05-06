0a81ea39d2360db839f573d29e800e4e4228c50d
kwonssshyeon <kwonsh4396@gmail.com>
2025-05-07 05:16:21 +0900

    [Parser] Refac: scanner 클래스 코드 정리 및 error 레벨 로깅 추가

--- parser/src/io/versionpulse/scanner/ApiGroupScanner.java
+++ parser/src/io/versionpulse/scanner/ApiGroupScanner.java
@@ -1,7 +1,6 @@
 package io.versionpulse.scanner;
 
 import java.io.IOException;
-import java.lang.annotation.Annotation;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.DirectoryStream;
@@ -12,69 +11,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.versionpulse.annotation.ApiGroup;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ApiGroupScanner {
-	
-	private String packageName;
-	
+
+	private final String packageName;
+
 	public ApiGroupScanner(String packageName) {
 		this.packageName = packageName;
 	}
-	
+
 	public List<Class<?>> execute() {
 		return scan(packageName);
 	}
-	
-	
+
 	private List<Class<?>> scan(String packageName) {
-	    List<Class<?>> result = new ArrayList<>();
-	    
-	    // 패키지 이름을 경로 형식으로 변환 ("com.example" -> "com/example")
-	    String packagePath = packageName.replace('.', '/');
-	    // 패키지의 URL 변환
-	    URL packageURL = ApiGroupScanner.class.getClassLoader().getResource(packagePath);
+		List<Class<?>> result = new ArrayList<>();
+		// 패키지 이름을 경로 형식으로 변환 ("com.example" -> "com/example")
+		String packagePath = packageName.replace('.', '/');
+		// 패키지의 URL 변환
+		URL packageURL = ApiGroupScanner.class.getClassLoader().getResource(packagePath);
 
-	    try {
-	        Path path = Paths.get(packageURL.toURI());
-	        
-	        if (!Files.isDirectory(path)) {
-	            return null;
-	        }
-	        
-	        DirectoryStream<Path> dirStream = Files.newDirectoryStream(path);
-	        // 디렉터리 내의 모든 파일을 처리
-	        for (Path entry : dirStream) {
-	            processEntry(entry, packageName, result);
-	        }
-	    } catch (URISyntaxException | IOException | ClassNotFoundException e) {
-	        e.printStackTrace();
-	    }
-	    
-	    return result;
+		if (packageURL == null) {
+			log.error("Package not found: {}", packageName);
+			return result;
+		}
+
+		try {
+			Path path = Paths.get(packageURL.toURI());
+			try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(path)) {
+				// 디렉토리 내의 모든 파일을 처리
+				for (Path entry : dirStream) {
+					processEntry(entry, packageName, result);
+				}
+			}
+		} catch (URISyntaxException | IOException | ClassNotFoundException e) {
+			log.error("Error scanning package {}: {}", packageName, e.getMessage());
+		}
+
+		return result;
 	}
 
-	
+
 	private void processEntry(Path entry, String packageName, List<Class<?>> result) throws ClassNotFoundException {
-		// 하위 디렉터리가 있다면 재귀적으로 탐색
-	    if (Files.isDirectory(entry)) {
-	    	String subPackageName = packageName + "." + entry.getFileName();
-		    result.addAll(scan(subPackageName));
-	    } 
-	    // .class 파일이라면 클래스 로딩
-	    else if (entry.toString().endsWith(".class")) {
-	    	String className = entry.getFileName().toString().substring(0, entry.getFileName().toString().length() - 6);
-		    Class<?> clazz = Class.forName(packageName + "." + className);
-		    if (isTargetClass(clazz)) {
-		        result.add(clazz);
-		    }
-	    }
-	}
-
-
-	// ApiGroup 어노테이션이 있는 클래스만 포함
-	private boolean isTargetClass(Class<?> clazz) {
-		Class<? extends Annotation> annotationClass = ApiGroup.class;
-		return clazz.isAnnotationPresent(annotationClass);
+		// 디렉토리인 경우
+		if (Files.isDirectory(entry)) {
+			String subPackageName = packageName + "." + entry.getFileName();
+			result.addAll(scan(subPackageName));
+		}
+		// 클래스 파일인 경우
+		else if (entry.toString().endsWith(".class")) {
+			String className = entry.getFileName().toString().substring(0, entry.getFileName().toString().length() - 6);
+			Class<?> clazz = Class.forName(packageName + "." + className);
+			if (clazz.isAnnotationPresent(ApiGroup.class)) {
+				result.add(clazz);
+			}
+		}
 	}
 }
-
--- parser/src/io/versionpulse/scanner/ApiMethodScanner.java
+++ parser/src/io/versionpulse/scanner/ApiMethodScanner.java
@@ -1,6 +1,5 @@
 package io.versionpulse.scanner;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,24 +11,11 @@ public class ApiMethodScanner {
 	
 	public List<Method> execute(Class<?> clazz) {
 		List<Method> result = new ArrayList<>();
-		
-		Method[] methods = clazz.getDeclaredMethods();
-		for (Method method : methods) {
-			if (isTargetMethod(method)) {
+		for (Method method : clazz.getDeclaredMethods()) {
+			if (method.isAnnotationPresent(Api.class)) {
 				result.add(method);
 			}
 		}
 		return result;
-	}
-	
-	// Api 어노테이션이 있는 메소드만 포함
-	private boolean isTargetMethod(Method method) {
-		Annotation[] annotations = method.getDeclaredAnnotations();
-		for (Annotation annotation : annotations) {
-			if (annotation instanceof Api) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
