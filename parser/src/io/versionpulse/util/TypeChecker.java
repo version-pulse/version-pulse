3e1a096a5c7bbb81aa7c5a281ccc079fa67009fd
kwonssshyeon <kwonsh4396@gmail.com>
2025-05-07 05:46:17 +0900

    [Parser] Fix: Collection의 구현체일 경우도 필터링되도록 수정

--- parser/src/io/versionpulse/util/TypeChecker.java
+++ parser/src/io/versionpulse/util/TypeChecker.java
@@ -2,23 +2,28 @@ package io.versionpulse.util;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.Map;
 
 public class TypeChecker {
 
 	public static ClassType check(Type type) {
+		// 일반 클래스 타입
 		if (type instanceof Class<?> clazz) {
 			if (clazz.getPackage() == null || clazz.isPrimitive()) {
 				return ClassType.PRIMITIVE;
 			} else if (clazz.getPackage().getName().startsWith("java.lang")) {
 				return ClassType.WRAPPER;
-			} else if (clazz.getName().startsWith("java.util.Map")) {
+			} else if (Map.class.isAssignableFrom(clazz)) {
 				return ClassType.MAP;
-			} else if (clazz.getName().startsWith("java.util")) {
+			} else if (Collection.class.isAssignableFrom(clazz)) {
 				return ClassType.LIST;
 			} else {
 				return ClassType.USER_DEFINED;
 			}
-		} else if (type instanceof ParameterizedType parameterizedType) {
+		}
+		// 제네릭 타입
+		else if (type instanceof ParameterizedType parameterizedType) {
 			Type rawType = parameterizedType.getRawType();
 			if (rawType instanceof Class<?> rawClazz) {
 				return check(rawClazz);
